f4d1ad9b2cabf24e2bfa194a809e45581427fdbc
Premm98 <prakashprem.pp2@gmail.com>
2024-02-23 22:03:35 +0530

    Separated each item in different file and caleed them

---
+++ lib/hourly_forecast_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HourlyForecastItem extends StatelessWidget {
+  const HourlyForecastItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      child: Container(
+        width: 100,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(18)),
+        padding: const EdgeInsets.all(8.0),
+        child: const Column(
+          children: [
+            Text(
+              '03:00',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Icon(
+              Icons.cloud,
+              size: 32,
+            ),
+            SizedBox(height: 10),
+            Text(
+              '320.12',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
